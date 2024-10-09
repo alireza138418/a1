f6073434b3eb55b79bc25eb685d1116a155422ed
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-09 21:34:01 +0330

    change default user and data

--- app2/app2/settings.py
+++ app2/app2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'app2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app2',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -122,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app2/core/models.py
+++ app2/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_field):
+        if not email:
+            raise ValueError('user ,ost have some email address')
+
+        user = self.model(email = self.normalize_email(email), **extra_field)
+        # add some validation for password
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        if not email:
+            raise ValueError('user ,ost have some email address')
+
+        # user = self.model(email = self.normalize_email(email), password = password)
+        user = self.create_user(email , password)
+        # add some validation for password
+        # user.set_passwoord(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        return  user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    f_name = models.CharField(max_length=255)
+    l_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    birth_date = models.DateField(null=True , blank=True) # image
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) # admin
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+
