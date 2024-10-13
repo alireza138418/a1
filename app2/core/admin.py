6c5a17e72aae0f05cf57cbafc3ab8c107b1e06c2
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-14 00:06:17 +0330

    improve user admin

--- app2/core/admin.py
+++ app2/core/admin.py
@@ -1,7 +1,20 @@
+from django.contrib.admin import ModelAdmin as BaseModelAdmin
 from django.contrib import admin
+from django.utils.translation import gettext as _
 from .models import *
 
-admin.site.register(User)
+class UserModelAmin(BaseModelAdmin):
+    ordering = ['id']
+    list_display = ['email' , 'f_name' , 'l_name']
+    fieldsets = (
+        (_('Login Information') , {'fields': ('email' , 'password')}),
+        (_('Personal Information'), {'fields': ('f_name', 'l_name', 'phone_number')}),
+        (_('Permission'), {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        (_('Dates'), {'fields': ('birth_date', 'last_login')}),
+    )
+
+
+admin.site.register(User , UserModelAmin)
 admin.site.register(Hotel)
 admin.site.register(Room)
 admin.site.register(Reservation)
