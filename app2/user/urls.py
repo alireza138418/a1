c813f88d6ffca2d31da1250a58203a2536f2c762
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-14 20:47:19 +0330

    add user management AIPs

--- app2/user/urls.py
+++ app2/user/urls.py
@@ -4,5 +4,8 @@ from user.views import *
 app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name = 'create'),
-
+    path('get/<str:email>', RetrieveUserView.as_view(), name = 'get'),
+    path('list/', ListUserView.as_view(), name = 'list'),
+    path('update/<str:email>', UpdateUserView.as_view(), name = 'update'),
+    path('delete/<str:email>', DeleteUserView.as_view(), name = 'delete'),
 ]
--- app2/user/views.py
+++ app2/user/views.py
@@ -1,8 +1,31 @@
-# from django.shortcuts import render
-# Create your views here
-
-from rest_framework import generics
-from .serializers import UserSerializers
+from rest_framework import generics , status
+from rest_framework.response import Response
+from .serializers import *
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializers
+
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = get_user_model().objects.all()
+    lookup_field = 'email'
+
+class ListUserView(generics.ListAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = get_user_model().objects.all()
+
+class UpdateUserView(generics.UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    queryset = get_user_model().objects.all()
+    lookup_field = 'email'
+
+class DeleteUserView(generics.DestroyAPIView):
+    queryset = get_user_model().objects.all()
+    lookup_field = 'email'
+
+    def destroy(self, request, *args, **kwargs):
+    #   self.queryset.filter(email = kwargs['email'])
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'message' : 'user deleted successfully!'}, status.HTTP_200_OK)
