ec976406e8a2aa8f21c5c00c467a8e6a1a5b6def
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-18 02:48:13 +0330

    add authentication to user AIPs

--- app2/user/urls.py
+++ app2/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from user.views import *
 
 app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name = 'create'),
+    path('login/', obtain_auth_token, name = 'token'),
     path('get/<str:email>', RetrieveUserView.as_view(), name = 'get'),
     path('list/', ListUserView.as_view(), name = 'list'),
     path('update/<str:email>', UpdateUserView.as_view(), name = 'update'),
