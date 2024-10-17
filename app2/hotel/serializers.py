71da690c440971a4452417e787a82aca178c2093
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-17 22:39:18 +0330

    add update, delete and list view

--- app2/hotel/serializers.py
+++ app2/hotel/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
-from core.models import Hotel
+from core.models import Hotel, Room
+
+class RoomSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'room_number', 'price', 'type')
 
 class HotelSerializers(serializers.ModelSerializer):
 
+    rooms = RoomSerializers(many=True)
     class Meta:
         model = Hotel
         fields = '__all__'
+
+    def create(self, validated_data):
+        rooms_data = validated_data.pop('rooms' , None)
+        hotel = Hotel.objects.create(**validated_data)
+        for room_data in rooms_data:
+            Room.objects.create(hotel=hotel, **room_data)
+
+        return hotel
+
+    def update(self, instance, validated_data):
+        rooms_date = validated_data.pop('rooms')
+        hotel = super.update(instance, validated_data)
+        for room_date in rooms_date:
+            room_instance = Room.objects.get(id=room_date['id'])
+            room_instance.update(**room_date)
+            room_instance.save()
+
+        return hotel
+
--- app2/hotel/views.py
+++ app2/hotel/views.py
@@ -1,8 +1,13 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 from .serializers import *
 
-class HotelViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+class HotelViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     serializer_class = HotelSerializers
     queryset = Hotel.objects.all()
 
-
+    def destroy(self, request, *args, **kwargs):
+    #   self.queryset.filter(email = kwargs['email'])
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'message' : 'hotel deleted successfully!'}, status.HTTP_200_OK)
