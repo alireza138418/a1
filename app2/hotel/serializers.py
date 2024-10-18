61d0efecdc15887af21111995980ed8b8c1aa182
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-18 18:13:29 +0330

    fix update hotel api bugs

--- app2/hotel/serializers.py
+++ app2/hotel/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from core.models import Hotel, Room
 
 class RoomSerializers(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    
     class Meta:
         model = Room
         fields = ('id', 'room_number', 'price', 'type')
@@ -29,5 +31,17 @@ class HotelSerializers(serializers.ModelSerializer):
             room_instance.update(**room_date)
             room_instance.save()
 
-        return hotel
+        rooms_date = validated_data.pop('rooms', None)
+        hotel = super().update(instance, validated_data)
+        for room_date in rooms_date:
+            room_instance = Room.objects.get(id=room_date.pop('id', None))
+            room_serializer = RoomSerializers(instance=room_instance, data=room_date, partial=True)
+            if room_serializer.is_valid():
+                room_serializer.save()
+            else:
+                raise serializers.ValidationError(room_serializer.errors)
 
+#           room_instance.update(room_date)
+#           room_instance.save()
+
+        return hotel
