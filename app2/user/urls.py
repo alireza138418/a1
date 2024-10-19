5266f52dc5486d4141ea160455930be1738792ce
alireza138418 <alireza.h.13841384@gmail.com>
2024-10-20 02:19:25 +0330

    add jwt api

--- app2/user/urls.py
+++ app2/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 from user.views import *
 
 app_name = 'user'
@@ -7,6 +8,9 @@ urlpatterns = [
     path('create/', CreateUserView.as_view(), name = 'create'),
     path('login/', obtain_auth_token, name = 'token'),
     path('logout/', LogoutView.as_view(), name = 'logout'),
+    path('jwtlogin/', TokenObtainPairView.as_view(), name='jwtlogin'),
+    path('jwtlogin/refresh/', TokenRefreshView.as_view(), name='jwtrefresh'),
+    path('jwtlogout/', TokenBlacklistView.as_view(), name='jwtlogout'),
     path('get/<str:email>', RetrieveUserView.as_view(), name = 'get'),
     path('list/', ListUserView.as_view(), name = 'list'),
     path('update/<str:email>', UpdateUserView.as_view(), name = 'update'),
